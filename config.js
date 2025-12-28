66a4eeaed3f0a0e33b285fea0f9dd5d4820a6fc3
GGBSSHOP <puskarpuskara2008@gmail.com>
2025-12-28 22:45:17 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// это пример на практике надо использовать свой/
+window.CONFIG = {
+    TELEGRAM_BOT_TOKEN: '6013096512:AAHjIIcrCvBwioUQroapULsV29lYreZhK-Y',
+    TELEGRAM_CHAT_ID: '713847629'
+};
