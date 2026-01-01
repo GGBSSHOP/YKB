c07881ac980d1af6bd2b494b9abe69963b1da3ea
GGBSSHOP <puskarpuskara2008@gmail.com>
2026-01-01 23:11:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// это пример на практике надо использовать свой/
+// это пример на практике надо использовать свои!
 window.CONFIG = {
     TELEGRAM_BOT_TOKEN: '6013096512:AAHjIIcrCvBwioUQroapULsV29lYreZhK-Y',
     TELEGRAM_CHAT_ID: '713847629'
